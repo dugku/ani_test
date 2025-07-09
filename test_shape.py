092600247394ade58ca2babba03d320b699c40bf
Mike <iphonerespon@gmail.com>
2025-07-09 06:49:05 -0500

    Works?

--- test_shape.py
+++ test_shape.py
@@ -7,12 +7,14 @@ from shapely.plotting import plot_points, plot_polygon
 coords = ((20, 50), (30,30), (80, 15), (95, 45), (64, 80), (25,67))
 polygon = MultiPoint(coords)
 
-voro = voronoi_polygons(polygon, only_edges=True)
+voro = voronoi_polygons(polygon,  extend_to=polygon.envelope)
 
 fig, ax = plt.subplots()
 
-plot_points(polygon, ax=ax, color="red", zorder=3)
 for poly in voro.geoms:
-    plot_polygon(poly, ax=ax, facecolor="none", edgecolor="black")
+    x, y = poly.exterior.xy
+    ax.fill(x , y, alpha=0.2, edgecolor='black')
+
+plot_points(polygon, ax=ax, color="blue")
 
 plt.show()
